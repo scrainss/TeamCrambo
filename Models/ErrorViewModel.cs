9318eb1b74617ec944881f0716b32aba9d2b5183
Jacob Klucher <klucher@etsu.edu>
2023-10-20 16:40:24 -0400

    Created default website template

--- Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace PokéPortal.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
